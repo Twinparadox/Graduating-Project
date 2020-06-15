c7725acf3fc72b38f1adfda780c5697a2b4ac627
Wonwoo Nam <nww731@gmail.com>
2020-06-16 00:48:08 +0900

    Init REST-API branch

--- trading-bot-master/api.py
+++ trading-bot-master/api.py
@@ -37,6 +37,7 @@ from flask_restful import reqparse
 
 app = Flask(__name__)
 api = Api(app)
+end = False
 
 class CreateUser(Resource):
     def post(self):
@@ -54,7 +55,24 @@ class CreateUser(Resource):
         except Exception as e:
             return {'error': str(e)}
 
+class Trader(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('email', type=str)
+            parser.add_argument('user_name', type=str)
+            parser.add_argument('password', type=str)
+            args = parser.parse_args()
+
+            _userEmail = args['email']
+            _userName = args['user_name']
+            _userPassword = args['password']
+            return {'Email': args['email'], 'UserName': args['user_name'], 'Password': args['password']}
+        except Exception as e:
+            return {'error': str(e)}
+
 api.add_resource(CreateUser, '/user')
+api.add_resource(Trader, '/trader')
 
 
 ### RL Trader ###
