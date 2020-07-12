6dcef260e2c7c227e85ee9c364cfcb8886300472
WonWoo Nam <nww731@gmail.com>
2020-07-12 23:57:25 +0900

    Add WebApp

---
+++ WebApp/routes/show-all-data.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var mysql_dbc = require('../config/database')();
+var connection = mysql_dbc.init();
+
+router.get('/', function(req, res, next) {
+    var sql = "SELECT * FROM trading_logs";
+    connection.query(sql, function(error, rows, fields) {
+        if(!error) {
+            for(var i = 0;i < rows.length;i++) {
+                console.log(rows[i]);
+            }
+        } else {
+            console.log("query error : "+error);
+        }
+    })
+})
+
+module.exports = router;
