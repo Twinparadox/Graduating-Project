5adee817823a5541e7c559df12b7d10b01d273ed
Wonwoo Nam <nww731@gmail.com>
2020-07-18 23:15:00 +0900

    Update WebApp
    DB 조회 기능 추가
    디자인 수정 및 트레이딩 봇 서비스 방식 논의 필요

--- WebApp/app.js
+++ WebApp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var showDataRouter = require('./routes/showData');
 var showAllDataRouter = require('./routes/show-all-data');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/all', showAllDataRouter);
+app.use('/log', showDataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- WebApp/routes/index.js
+++ WebApp/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Reinforcement Trader' });
 });
 
 /* POST home page, */
 router.post('/', function(req, res, next) {
-    res.render('index', {title:'Express'});
+    res.render('index', {title:'Reinforcement Trader'});
 })
 
 module.exports = router;
